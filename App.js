e3335c009be11dd60e18e6cd64615d892c4c36e9
Rafael Ramos <rafael_darknees@hotmail.com>
2020-07-17 16:56:54 -0300

    app.js finished

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
-import { StatusBar } from 'expo-status-bar';
+import { PersistGate } from 'redux-persist/es/integration/react';
 
 import Home from './src/pages/home';
+import { store, persistedStore } from './src/store/createStore';
 
 export default function App() {
   return (
-    <>
-      <Home />
-      <StatusBar style="auto" />
-    </>
+    <Provider store={store}>
+      <PersistGate persistor={persistedStore}>
+        <Home />
+      </PersistGate>
+    </Provider>
   );
 }
