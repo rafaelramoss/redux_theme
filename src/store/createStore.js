17727287318c612d1edcd73a8662b0071cf9a47e
Rafael Ramos <rafael_darknees@hotmail.com>
2020-07-17 16:46:40 -0300

    creating storage

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,1 +1,15 @@
-import {} from 'redux';
+import AsyncStorage from '@react-native-community/async-storage';
+import { createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import { PersistGate } from 'redux-persist/es/integration/react';
+
+import rootReducer from './modules/reducers/themeReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['themeReducer'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(rootReducer);
