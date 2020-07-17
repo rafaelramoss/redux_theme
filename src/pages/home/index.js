525a79e458abaf63dee508066e71bb867d9397e7
Rafael Ramos <rafael_darknees@hotmail.com>
2020-07-17 17:21:44 -0300

    page home finished

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { ThemeProvider } from 'styled-components';
 
-import switchTheme from '../../store/modules/actions';
+import { switchTheme } from '../../store/modules/actions';
+import { lightTheme, darkTheme } from '../../themes';
 import {
   Container,
   ContainerHeader,
@@ -14,17 +16,28 @@ import {
 } from './style';
 
 export default function Home() {
+  const theme = useSelector((state) => state.themeReducer.theme);
+  const dispatch = useDispatch();
   return (
-    <Container>
-      <ContainerHeader>
-        <Texto>Hey, what´s up?!</Texto>
-        <Texto> Click in the button for change the theme</Texto>
-      </ContainerHeader>
-      <ContainerButton>
-        <Button>
-          <TextButton>Click me :)</TextButton>
-        </Button>
-      </ContainerButton>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <StatusBar barStyle={theme.STATUS_BAR} />
+      <Container>
+        <ContainerHeader>
+          <Texto>Hey, what´s up?!</Texto>
+          <Texto> Click in the button for change the theme</Texto>
+        </ContainerHeader>
+        <ContainerButton>
+          {theme.mode === 'light' ? (
+            <Button onPress={() => dispatch(switchTheme(darkTheme))}>
+              <TextButton>Change to Dark Theme</TextButton>
+            </Button>
+          ) : (
+            <Button onPress={() => dispatch(switchTheme(lightTheme))}>
+              <TextButton>Change to Light Theme</TextButton>
+            </Button>
+          )}
+        </ContainerButton>
+      </Container>
+    </ThemeProvider>
   );
 }
