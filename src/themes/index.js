29df6f64d64405b975f7e96db0e1cea6dce97786
Rafael Ramos <rafael_darknees@hotmail.com>
2020-07-17 16:29:02 -0300

    added themes

---
+++ src/themes/index.js
@@ -0,0 +1,19 @@
+const darkTheme = {
+  mode: 'dark',
+  PRIMARY_BACKGROUND_COLOR: '#212121',
+  PRIMARY_TEXT_COLOR: '#fff',
+  PRIMARY_BUTTON_COLOR: 'orange',
+  PRIMARY_TEXT_BUTTON_COLOR: '#fff',
+  STATUS_BAR: 'light-contente',
+};
+
+const lightTheme = {
+  mode: 'light',
+  PRIMARY_BACKGROUND_COLOR: '#fff',
+  PRIMARY_TEXT_COLOR: '#212121',
+  PRIMARY_BUTTON_COLOR: 'pink',
+  PRIMARY_TEXT_BUTTON_COLOR: '#fff',
+  STATUS_BAR: 'default',
+};
+
+export { darkTheme, lightTheme };
